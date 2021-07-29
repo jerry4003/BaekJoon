58aecdbf7cb848bd90c4cc7a10c803691a1b735b
정희상 <jerry400310@gmail.com>
2021-07-29 19:28:02 +0900

    second

--- 백준/1546.py
+++ 백준/1546.py
@@ -1,1 +1,8 @@
-sdas
+N=int(input())
+score=list(map(float,input().split()))
+cnt=0.0
+Mscore=max(score)
+for i in range(N):
+    score[i]=score[i]/Mscore*100.0
+    cnt+=score[i]
+print(cnt/float(N))
